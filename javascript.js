5a53a63f59b3987bdb5064a4cb6c5f8c90e179d1
Austin T <austin.taranto@gmail.com>
2019-11-24 17:02:41 -0500

    min hand mistake fixed

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ const hourHand = document.querySelector(".hour-hand");
 function setDate() {
     // console.log("hi");
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
@@ -12,8 +13,11 @@ function setDate() {
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + 90;
-    minHand.style.transform = `rotate(${minsDegrees}deg)`
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`
     
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
 }
 
 setInterval(setDate, 1000);
